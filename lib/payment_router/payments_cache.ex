7ccccfe4b664b8aeca5d2395b06c4967e752f42f
Caio Reidt <31038200+qreidt@users.noreply.github.com>
2025-07-02 22:21:12 -0300

    Add TTL to cache layer

--- lib/payment_router/payments_cache.ex
+++ lib/payment_router/payments_cache.ex
@@ -2,6 +2,7 @@ defmodule PaymentRouter.PaymentsCache do
   require Logger
   use GenServer
 
+  @ttl_ms 10_000
   @table :payments_cache
 
   # Client API
@@ -12,7 +13,7 @@ defmodule PaymentRouter.PaymentsCache do
 
   def get(uuid) do
     case :ets.lookup(@table, uuid) do
-      [{^uuid, payment}] ->
+      [{^uuid, payment, _}] ->
         Logger.info("Cache HIT: #{@table}:#{uuid}")
         {:ok, payment}
       [] ->
@@ -22,7 +23,7 @@ defmodule PaymentRouter.PaymentsCache do
   end
 
   def put(uuid, payment) do
-    :ets.insert(@table, {uuid, payment})
+    :ets.insert(@table, {uuid, payment, now_ms()})
     Logger.info("Cache PUT: #{@table}:#{uuid}")
     :ok
   end
@@ -30,7 +31,39 @@ defmodule PaymentRouter.PaymentsCache do
   # Server callbacks
 
   def init(_) do
+    # Create ETS table
     :ets.new(@table, [:named_table, :set, :public, read_concurrency: true])
+
+    # Start cleanup Routine
+    schedule_cleanup()
+
     {:ok, %{}}
+  end
+
+  def handle_info(:cleanup, state) do
+    Logger.info("Running cache table cleanup: #{@table}")
+
+    cleanup_expired()
+    schedule_cleanup()
+    {:noreply, state}
+  end
+
+  # Helpers
+
+  defp now_ms, do: System.system_time(:millisecond)
+
+  defp cleanup_expired do
+    now = now_ms()
+    for {uuid, _payment, inserted_at} <- :ets.tab2list(@table),
+        now - inserted_at >= @ttl_ms do
+
+      Logger.info("Cache Key Expired: #{@table}:#{uuid}")
+      :ets.delete(@table, uuid)
+    end
+  end
+
+  defp schedule_cleanup do
+    Process.send_after(self(), :cleanup, @ttl_ms)
+    nil
   end
 end
